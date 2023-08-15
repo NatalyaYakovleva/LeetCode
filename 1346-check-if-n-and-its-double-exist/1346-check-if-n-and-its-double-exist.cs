5db5567eb3d9f8508e29fcd47edef5d9d4024401
NatalyaYakovleva <141887822+NatalyaYakovleva@users.noreply.github.com>
2023-08-15 14:47:14 +0700

    Time: 96 ms (66.87%), Space: 41.1 MB (38.39%) - LeetHub

---
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public bool CheckIfExist(int[] arr) {
+        for (int i = 0; i < arr.Length; i++) {
+            for (int j = 0; j < arr.Length; j++) {
+                if (i != j) {
+                    if (arr[i] * 2 == arr[j]) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
