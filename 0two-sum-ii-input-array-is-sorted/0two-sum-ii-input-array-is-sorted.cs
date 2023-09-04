755208a209f34426d05720ae1d9eb041d83f63f7
NatalyaYakovleva <141887822+NatalyaYakovleva@users.noreply.github.com>
2023-09-04 12:15:41 +0700

    Time: 146 ms, Memory: 46.5 MB - LeetHub

---
+++ 0two-sum-ii-input-array-is-sorted/0two-sum-ii-input-array-is-sorted.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int[] TwoSum(int[] numbers, int target) {
+        int i = 1, j = numbers.Length;
+        while (i < j) {
+            int x = numbers[i - 1] + numbers[j - 1];
+            if (x == target) {
+                return new int[] {i, j};
+            }
+            if (x < target) {
+                ++i;
+            } else {
+                --j;
+            }
+        }
+        return new int[] {-1, -1};
+    }
+}
